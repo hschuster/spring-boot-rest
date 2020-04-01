9f5759a518f719984003fd4dd85fa98cf30313c5
Schuster <hschuster.xo@gmail.com>
2020-04-01 12:59:02 +0200

    Verschönerungen....

--- src/main/java/at/immodat/test/springbootrest/SpringRestController.java
+++ src/main/java/at/immodat/test/springbootrest/SpringRestController.java
@@ -5,8 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.web.bind.annotation.RestController
+@RestController
 public class SpringRestController {
 
 
